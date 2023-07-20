23ed9d4130c398d24665d69b646a791b74409f43
Ford Cullen (fordnhc) <ford.nhc@gmail.com>
2023-07-20 14:23:59 -0700

    added js and scroll transparency

--- website_w_font.js
+++ website_w_font.js
@@ -1,3 +1,16 @@
-window.addEventListener('scroll', () => {
-    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-  }, false);
+$(window).scroll(function() {
+  var scrollTop = $(this).scrollTop();
+
+  $('.profile').css({
+    opacity: function() {
+      var elementHeight = $(this).height()
+      return 0 + (elementHeight - scrollTop ) / elementHeight;
+    }
+  }),
+  $('.gradient').css({
+    opacity: function() {
+        var elementHeight = $(this).height()
+        return 1 - (elementHeight - scrollTop ) / elementHeight;
+      }
+  })
+});
