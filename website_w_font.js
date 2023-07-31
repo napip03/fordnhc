8ea40995ad8f922e14091b227ab9c247f6587963
Ford Cullen (fordnhc) <ford.nhc@gmail.com>
2023-07-31 11:49:34 -0700

    july 31

--- website_w_font.js
+++ website_w_font.js
@@ -12,5 +12,22 @@ $(window).scroll(function() {
         var elementHeight = $(this).height()
         return 1 - (elementHeight - scrollTop ) / elementHeight;
       }
-  })
+  });
+});
+$(window).scroll(function () {
+  console.log($(window).scrollTop())
+  if ($(window).scrollTop() > 63) {
+    $('.navbar').addClass('navbar-fixed');
+  }
+  if ($(window).scrollTop() < 64) {
+    $('.navbar').removeClass('navbar-fixed');
+  }
+});
+$(window).scroll(function(){
+  if($(this).scrollTop() > 400){
+    $('.scroll-header').css({'display': 'none'});
+  }
+  if($(this).scrollTop() < 400){
+    $('.scroll-header').css({'display': 'inline'});
+  }
 });
